ba023969fae4f7259f686db0e1cd1405d928875d
Maijq <1648639361@qq.com>
2021-01-31 00:37:12 +0800

    rank完善

--- components/book-intro/book-intro.vue
+++ components/book-intro/book-intro.vue
@@ -1,47 +1,51 @@
 <template>
 	<view class="">
-		<view class="main-box" v-for="(item,index) in listSub" :key="index" @click="goDetails(item.bookId)">
-			<view class="image">
-				<image :src="item.src" mode=""></image>
-			</view>
+		<view class="main-box" v-for="(item, index) in listSub" :key="index" @click="goDetails(item.bookId)">
+			<view class="image"><image :src="item.src" mode=""></image></view>
 			<view class="datils">
-				<text class="title">{{item.bookTitle}}</text>
+				<text class="title">{{ item.bookTitle }}</text>
 				<view class="intro">
-					详情一键三连点赞Mark关注该博主随时了解T赞Mark关注该博主随时了解T要输入用户名和密赞Mark关注该博主随时了解T要输入用户名和1231313密要输入用户名和密码的解决方法asdfas的博asdfasdfasddasfasdfas客 2614
+					详情一键三连点赞Mark关注该博主随时了解T赞Mark关注该博主随时了解T要输入用户名和密赞Mark关注该博主随时了解T要输入用户名和1231313密要输入用户名和密码的解决方法asdfas的博asdfasdfasddasfasdfas客
+					2614
 				</view>
-				<view class="msg"> {{item.author}} · {{item.type}} · {{item.state}} </view>
+				<view class="msg">{{ item.author }} · {{ item.type }} · {{ item.state }}</view>
 			</view>
 		</view>
 	</view>
-	
-	
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
+export default {
+	data() {
+		return {};
+	},
+	props: {
+		list: {
+			type: Array,
+			default: []
 		},
-		props:{
-			list:Array,
-			default:[]
-		},
-		methods:{
-			// 跳转详情
-			goDetails(id){
-				uni.navigateTo({
-					url:'../../pages/bookDatiles/bookDatiles?id='+id
-				})
+		limit: {
+			type: Number,
+			default: 3
+		}
+	},
+	methods: {
+		// 跳转详情
+		goDetails(id) {
+			uni.navigateTo({
+				url: '../../pages/bookDatiles/bookDatiles?id=' + id
+			});
+		}
+	},
+	computed: {
+		listSub() {
+			if(this.limit === 0){
+				return this.list
 			}
-		},
-		computed:{
-			listSub() {
-				return this.list.slice(0,3)
-			}
+			return this.list.slice(0, this.limit);
 		}
 	}
+};
 </script>
 
 <style lang="less" scoped>
@@ -53,24 +57,25 @@
 	// background-color: #808080;
 	padding: 20rpx 0;
 	.image {
-		height:100%;
+		height: 100%;
 		width: 23%;
+		margin-right: 30rpx;
 		image {
 			height: 100%;
 			width: 160rpx;
 			border-radius: 20rpx;
 		}
 	}
-	.datils{
-		height:100%;
+	.datils {
+		height: 100%;
 		// width: 77%;
-		padding: 0 0 0 30rpx;
+		padding: 0 0 0 20rpx;
 		overflow: hidden;
-		.title{
+		.title {
 			color: #333;
 			font-size: 30rpx;
 		}
-		.intro{
+		.intro {
 			// height: 100rpx;
 			padding-top: 20rpx;
 			color: #999;
@@ -82,11 +87,10 @@
 			-webkit-line-clamp: 3;
 			-webkit-box-orient: vertical;
 		}
-		.msg{
+		.msg {
 			color: #999;
 			font-size: 20rpx;
 			padding-top: 10rpx;
-			
 		}
 	}
 }
--- pages/rank/rank.vue
+++ pages/rank/rank.vue
@@ -2,32 +2,40 @@
 	<view>
 		<uni-segmented-control :current="current" :values="items" styleType="text" @clickItem="onClickItem" activeColor="#ff6600"></uni-segmented-control>
 		<view class="content">
-			<view v-show="current === 0">
+			<view v-show="current === index1" class="content-item" v-for="(node,index1) in items" >
 				<view class="left-tab">
-					<view class="check-bth" :class="{'check-active': index===1}" v-for="(item,index) in list">{{item}}</view>
-					
+					<view v-show="index1===0" class="check-bth" :class="{'check-active': index==active}" v-for="(item,index) in list1 " :key="index" @click="changTab(index)">{{item}}</view>
+					<view v-show="index1===1" class="check-bth" :class="{'check-active': index==active}" v-for="(item,index) in list2" :key="index" @click="changTab(index)">{{item}}</view>
+					<view v-show="index1===2" class="check-bth" :class="{'check-active': index==active}" v-for="(item,index) in list3" :key="index" @click="changTab(index)">{{item}}</view>
+					<view v-show="index1===3" class="check-bth" :class="{'check-active': index==active}" v-for="(item,index) in list4" :key="index" @click="changTab(index)">{{item}}</view>
 				</view>
+				
 				<view class="right-box">
-					
+					<scroll-view scroll-y="true" class="scroll">
+						<view>
+							<book-intro :list="data" :limit="0"></book-intro>
+						</view>
+					</scroll-view>
 				</view>
 			</view>
-			<view v-show="current === 1">
-				选项卡2的内容
-			</view>
-			<view v-show="current === 2">
-				选项卡3的内容
-			</view>
+			
 		</view>
 	</view>
 </template>
 
 <script>
+	import {textList} from '../home/data.js'
 export default {
 	data() {
 		return {
 			items: ['总排行', '男生', '女生','教育'],
 			current: 0,
-			list:['流行榜', '免费','会员','完结','连载']
+			list1:['人气榜', '免费榜', '会员榜', '完结榜' ,'新书榜'],
+			list2:['男生人气', '免费','会员','完结','连载'],
+			list3:['女生人气', '免费','会员','完结','连载'],
+			list4:['热度榜', '教育','会员','完结','连载'],
+			active: 0,
+			data:textList
 		};
 	},
 	methods: {
@@ -35,6 +43,10 @@ export default {
 			if (this.current !== e.currentIndex) {
 				this.current = e.currentIndex;
 			}
+		},
+		//点击排行
+		changTab(index){
+			this.active = index
 		}
 	}
 };
@@ -42,24 +54,38 @@ export default {
 
 <style lang="less" scoped>
 	.content{
-		display: flex;
-		flex-direction: row;
-		background-color: #ffaaff;
-		.left-tab{
-			width: 25vw;
-			height: 90vh;
-			background-color: #ffffff;
-			.check-bth{
-				line-height: 75rpx;
-				height: 75rpx;
-				background-color: #ffaa7f;
-				
-				margin-top: 2rpx;
-				text-align: center;
+			
+		.content-item{
+			padding-top: 20rpx;
+			display: flex;
+			height: 88vh;
+			flex-direction: row;
+			.left-tab{
+				width: 25vw;
+				height: 100%;
+				background-color: #fafafa;
+				.check-bth{
+					line-height: 75rpx;
+					height: 75rpx;
+					background-color: #ffebc2;
+					
+					margin-top: 2rpx;
+					text-align: center;
+				}
+				.check-active{
+					border-left: 7rpx solid #ff6600;
+					color: #ff6600;
+				}
 			}
-			.check-active{
-				
+			.right-box{
+				width: 560rpx;
+				padding: 20rpx;
+				overflow: hidden;
+				.scroll{
+					height: 95%;
+				}
 			}
 		}
+		
 	}
 </style>

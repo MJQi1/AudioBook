0f821744f79cbe2eded898f1e564d624c023eb59
Maijq <1648639361@qq.com>
2021-02-04 00:29:40 +0800

    听书详情完善

--- pages/bookDatiles/bookDatiles.vue
+++ pages/bookDatiles/bookDatiles.vue
@@ -16,46 +16,63 @@
 		</view>
 
 		<uni-section title="简介" sub-title="" type="line"></uni-section>
-		<view class="install">
-			i是一种需求非常发散的产品，DCloud官方也无意用 uni-ui 压制第三方ui插件的空间， 但官方有义务在性能和多端方面提供一个开源的标杆给大家。
-			我们欢迎更多优秀的ui组件出现，也欢迎更多人贡献 uni-ui 的主题风格，满足更多用户的需求。
-		</view>
+		
+			<uni-group title="作者" margin-top="" mode="card">
+				<view class="nextText">
+					内容主体，可自定义内容及样式
+				</view>
+			</uni-group>
+			<uni-group title="主播" margin-top="" mode="card" >
+				<view class="nextText">
+					内容主体，可自定义内容及样式
+				</view>
+			</uni-group>
+			<uni-group title="内容简介" margin-top="" mode="card" >
+				<view class="nextText">
+					内容主体，可自定义内容及样式
+				</view>
+			</uni-group>
+	
+			
 		<uni-section title="全部章节" sub-title="" type="line"></uni-section>
 		<uni-list>
-			<uni-list-item title="第一章 打法的萨芬" note=""></uni-list-item>
-			<uni-list-item title="第二章 阿桑的歌发" note=""></uni-list-item>
+			<uni-list-item v-for="(item, index) in bookTemp" :key="index" :title="`第${index + 1}章  ${item.title}`" to="../home/search/search"></uni-list-item>
+			<uni-list-item title="全部章节" link to="xxx"></uni-list-item>
 		</uni-list>
 		<uni-section title="评价" sub-title="" type="line"></uni-section>
 		<view class="evalute">
 			<view class="evalute-grade">
 				<view class="grade">分数：2.5</view>
-				<uni-rate value="2.5" size="20" readonly=""></uni-rate>
+				<uni-rate value="2.5" size="20" readonly></uni-rate>
 			</view>
 			<view class="evalute-input">
 				<uni-forms ref="form">
 					<view class="input-grade">
 						<view class="grade">评论</view>
-						<uni-rate value="0" size="15"></uni-rate>
+						<uni-rate allowHalf="true" @change="getRate" :value="evaluteData.rate" size="15"></uni-rate>
 					</view>
-					<textarea value="" class="text-area" placeholder="" />
-					<button size="mini" class="pinglun" form-type="submit">评论</button>
+					<textarea v-model="evaluteData.text" class="text-area" placeholder="说说你的想法..." />
+					<text style="color: red;font-size: 16rpx;" v-text="errMsg"></text>
+					<button size="mini" class="pinglun" @click="evalute">评论</button>
 				</uni-forms>
 			</view>
 			<view class="evalute-list">
 				<uni-list>
-					<uni-list-item title="列表左侧带略缩图" note="列表描述信息" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png"
-					     thumb-size="lg" rightText="右侧文字"></uni-list-item>
+					<uni-list-item
+						title="名字"
+						note="列表描述信息列表描述信息列表描述信息列表描述信息列表列表描述信息列表描述信息列表描述信列表描述信息列表描述信息列表描述信列表描述信息列表描述信息列表描述信描述信息列表描述信息列表描述信息"
+						thumb="https://tse3-mm.cn.bing.net/th/id/OIP.1NiQaNCRWX0sioJGRwKuAwHaIS?w=187&h=209&c=7&o=5&dpr=1.06&pid=1.7"
+						thumb-size="lg"
+						rightText="右侧文字"
+					>
+						<template slot="header">
+							<image style="width: 100rpx;height:100rpx" src="../../static/icons/xianxia.png" mode=""></image>
+						</template>
+						<template slot="footer">
+							<text style="font-size: 16rpx; color: #999;">2020-1-2</text>
+						</template>
+					</uni-list-item>
 				</uni-list>
-				<uni-list-item>
-				        <!-- 自定义 header -->
-				        <view slot="header" class="slot-box"><image class="slot-image" src="" mode="widthFix"></image></view>
-				        <!-- 自定义 body -->
-				        <text slot="body" class="slot-box slot-text">自定义插槽</text>
-				        <!-- 自定义 footer-->
-				        <template slot="footer">
-				            <image class="slot-image" src="" mode="widthFix"></image>
-				        </template>
-				    </uni-list-item>
 			</view>
 		</view>
 		<view>{{ bookId }}</view>
@@ -67,12 +84,78 @@ export default {
 	data() {
 		return {
 			bookId: '',
+			bookTemp: [
+				{
+					title: '阿里的父1',
+					link: 'src'
+				},
+				{
+					title: '阿父母公开2',
+					link: 'src'
+				},
+				{
+					title: '阿里的父母3',
+					link: 'src'
+				},
+				{
+					title: '阿里4',
+					link: 'src'
+				},
+				{
+					title: '阿5',
+					link: 'src'
+				},
+				{
+					title: '阿里的父母6',
+					link: 'src'
+				},
+				{
+					title: '阿里的父7',
+					link: 'src'
+				},
+				{
+					title: '阿里的8',
+					link: 'src'
+				},
+				{
+					title: '阿里的9',
+					link: 'src'
+				},
+				{
+					title: '阿里10',
+					link: 'src'
+				}
+			],
+			evaluteData: {
+				text: '',
+				rate: 0
+			},
+			errMsg:'',
 			src: 'http://imagev2.xmcdn.com/group74/M08/F7/E5/wKgO3F6ZKlyTkqKqAAMEKEhOSIw777.jpg!op_type=5&upload_type=album&device_type=ios&name=mobile_large&magick=png'
 		};
 	},
 	onLoad: function(option) {
 		//option为object类型，会序列化上个页面传递的参数
 		this.bookId = option.id; //打印出上个页面传递的参数。
+	},
+	methods: {
+		//获取评分
+		getRate(e) {
+			this.evaluteData.rate = e.value;
+		},
+		// 评论
+		evalute() {
+			if (this.evaluteData.text == '') {
+				this.errMsg = '请填写评论'
+			} else if(this.evaluteData.rate == 0) {
+				this.errMsg = '请进行评分'
+			}else{
+				console.log(`submit ${this.evaluteData}`);
+				
+				this.errMsg = ''
+				this.evaluteData = {title:'',rate:0}
+			}
+		}
 	}
 };
 </script>
@@ -118,6 +201,9 @@ export default {
 		}
 	}
 }
+.install {
+	padding: 30rpx;
+}
 .evalute {
 	padding: 30rpx;
 	.evalute-grade {
@@ -133,6 +219,7 @@ export default {
 	}
 	.evalute-input {
 		position: relative;
+		margin-bottom: 100rpx;
 		.input-grade {
 			display: flex;
 			justify-content: space-between;
@@ -156,5 +243,8 @@ export default {
 			color: #ffffff;
 		}
 	}
+}
+.nextText{
+	font-size: 20rpx;
 }
 </style>

661e7459db9660c8ae3f81736c1467ee71a7b559
Maijq <1648639361@qq.com>
2021-02-05 00:59:24 +0800

    背景图片更新

---
+++ pages/mine/chartMessage/chartMessage.vue
@@ -0,0 +1,48 @@
+<template>
+	<view>
+		<uni-segmented-control :current="current" styleType="text" activeColor="#eb9d00" :values="items" @clickItem="onClickItem"></uni-segmented-control>
+		<view class="content">
+			<view v-show="current === 0">
+				<uni-list>
+					<uni-list-item title="官方信息" note="活动点赞了你,活动点赞了你活动点赞了你,活动点赞了你,活动点赞了你,活动点赞了你" thumb="/static/uni.png" thumb-size="lg">
+						<template slot="footer">
+							<text style="font-size: 20rpx;color: #999;">2020-10-20</text>
+						</template>
+					</uni-list-item>
+				</uni-list>
+			</view>
+			<view v-show="current === 1">
+				<uni-list><uni-list-chat :avatar-circle="true" title="xxx" avatar="/static/uni.png" note="呵呵,你可真秀" time="2020-02-02 20:20"></uni-list-chat></uni-list>
+			</view>
+			<view v-show="current === 2">
+				<uni-list>
+					<uni-list-item title="官方信息" note="xxx点赞了你" thumb="/static/uni.png" thumb-size="lg">
+						<template slot="footer">
+							<text style="font-size: 20rpx;color: #999;">2020-10-20</text>
+						</template>
+					</uni-list-item>
+				</uni-list>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			items: ['系统消息', '评论', '点赞'],
+			current: 0
+		};
+	},
+	methods: {
+		onClickItem(e) {
+			if (this.current !== e.currentIndex) {
+				this.current = e.currentIndex;
+			}
+		}
+	}
+};
+</script>
+
+<style lang="less"></style>
---
+++ pages/mine/settings/settings.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<uni-list>
+			<uni-list-item title="检查更新" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="软件不错,赞一个" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="帮助中心" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="意见反馈" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="帐号注销" note=" " link="navigateTo"></uni-list-item>
+			
+			<uni-list-item title="隐私设置" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="关于Audio听书" note=" " link="navigateTo"></uni-list-item>
+			<uni-list-item title="清除缓存" note=" " link="navigateTo"></uni-list-item>
+			
+			<uni-list-item title="" note=""></uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less" scoped>
+
+</style>
